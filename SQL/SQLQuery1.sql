4405412f62b8c4965809297496f3070cdcc53ee5
Peggy Carambas <PCarambas@gmail.com>
2023-02-03 09:46:07 -0600

    Initial commit

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,65 @@
+﻿USE [master]
+
+IF db_id('FabricFinder') IS NULl
+  CREATE DATABASE [FabricFinder]
+GO
+
+USE [FabricFinder]
+GO
+
+
+DROP TABLE IF EXISTS [PatternFabric];
+DROP TABLE IF EXISTS [Fabric];
+DROP TABLE IF EXISTS [FabricType];
+DROP TABLE IF EXISTS [Pattern];
+DROP TABLE IF EXISTS [UserProfile];
+GO
+
+
+CREATE TABLE [Fabric] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Color] nvarchar(255) NOT NULL,
+  [Yardage] float,
+  [ImageUrl] image,
+  [UserId] int NOT NULL,
+  [FabricTypeId] int NOT NULL
+)
+GO
+
+CREATE TABLE [UserProfile] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [FirebaseUserId] nvarchar(255) UNIQUE,
+  [Email] nvarchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [Pattern] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Name] nvarchar(255) NOT NULL,
+  [ImageUrl] image
+)
+GO
+
+CREATE TABLE [PatternFabric] (
+  [PatternId] int,
+  [FabricId] int
+)
+GO
+
+CREATE TABLE [FabricType] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Yype] nvarchar(255) NOT NULL
+)
+GO
+
+ALTER TABLE [Fabric] ADD FOREIGN KEY ([FabricTypeId]) REFERENCES [FabricType] ([Id])
+GO
+
+ALTER TABLE [PatternFabric] ADD FOREIGN KEY ([FabricId]) REFERENCES [Fabric] ([Id]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [PatternFabric] ADD FOREIGN KEY ([PatternId]) REFERENCES [Pattern] ([id])
+GO
+
+ALTER TABLE [Fabric] ADD FOREIGN KEY ([UserId]) REFERENCES [UserProfile] ([Id])
+GO
---
+++ SQL/SQLQuery2.sql
@@ -0,0 +1,7 @@
+﻿USE [FabricFinder]
+GO
+
+INSERT INTO [Fabric]
+  ([Id], [Name], [Color], [Yardage], [ImageUrl], [UserId], [FabricTypeId])
+VALUES
+(1, 'Double Brushed Poly', 'Yellow', 3, 'https://cdn.shopify.com/s/files/1/2444/8261/products/20190930-122658_394x.jpg?v=1658189799', 1, 2),
